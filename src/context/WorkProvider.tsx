900ee0370be882f843a52996af2dd54e6d4b15ae
kordomakto <hakimsanny898@gmail.com>
2026-09-24 20:02:57 +0300

    Add WorkProvider context for managing workout plans and saved works

---
+++ src/context/WorkProvider.tsx
@@ -0,0 +1,39 @@
+import IWork from "@/types/workType";
+import React, {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+} from "react";
+
+interface IWorkProvider {
+  plans: IWork[];
+  setPlans: Dispatch<SetStateAction<IWork[]>>;
+  savedWorks: IWork[];
+  setSavedWorks: Dispatch<SetStateAction<IWork[]>>;
+}
+export const WorksProvider = createContext<IWorkProvider>({
+  plans: [],
+  setPlans: () => {},
+  savedWorks: [],
+  setSavedWorks: () => {},
+});
+
+const WorkProvider = ({ children }: { children: ReactNode }) => {
+  const [plans, setPlans] = useState([]);
+  const [savedWorks, setSavedWorks] = useState([]);
+  const contextData = {
+    plans,
+    setPlans,
+    savedWorks,
+    setSavedWorks,
+  };
+  return (
+    <WorksProvider.Provider value={contextData}>
+      {children}
+    </WorksProvider.Provider>
+  );
+};
+
+export default WorkProvider;
